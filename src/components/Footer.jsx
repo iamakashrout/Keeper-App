2e82646075db36fcd9b86e7061046faa2e94947a
Akash Rout <116869015+iamakashrout@users.noreply.github.com>
2023-01-26 16:44:20 +0530

    3rd Commit
    Implemented Add and Delete note features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Footer(){
-    const year=new Date().getFullYear();
-    return (
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
     <footer>
-        <p>Copyright © {year}</p>
+      <p>Copyright ⓒ {year}</p>
     </footer>
-    )
+  );
 }
 
 export default Footer;
